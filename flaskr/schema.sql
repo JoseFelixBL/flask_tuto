c9221915f91637957560c5f47880588d9e21905e
José Bello <jfbello.work@gmail.com>
2024-05-10 21:10:37 +0200

    Add post_like table to DB Schema

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,15 +1,23 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
-CREATE TABLE user (
+DROP TABLE IF EXISTS post_like;
+CREATE TABLE IF NOT EXISTS user (
     id INTEGER primary key autoincrement,
     username Text unique not null,
     password text not null
 );
-CREATE TABLE post (
+CREATE TABLE IF NOT EXISTS post (
     id INTEGER primary key autoincrement,
     author_id INTEGER not null,
     created timestamp not null default CURRENT_TIMESTAMP,
     title text not null,
     body text not null,
     foreign key (author_id) references user (id)
+);
+CREATE TABLE IF NOT EXISTS post_like (
+    id INTEGER PRIMARY KEY autoincrement,
+    post_id INTEGER not null,
+    author_id INTEGER not null,
+    foreign KEY (post_id) references post (id),
+    foreign KEY (author_id) references user (id)
 );
